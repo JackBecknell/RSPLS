7dc3463310e69fcdc5b489a0b323271107b69c87
JackBecknell <52moosefit@gmail.com>
2022-03-08 11:07:33 -0500

    Moved the generate gesture from the AiPlayer into Player. Also switched out the list of gestures for a dictionary with values of a win conditionstored to the key words.

--- player.py
+++ player.py
@@ -1,12 +1,17 @@
-
-
-
-
+import random
+#For the self.gestures_win_lose_dict the values stored to the keywords are the values the keyword can beat.
 class Player:
     def __init__(self) -> None:
-        self.gesture = ["lizard", "scissors", "paper", "rock", "spock"]
+        self.gestures_win_lose_dict = {
+    "scissors": ['paper','lizard'],
+    "paper" : ['rock','spock'],
+    "rock" : ['lizard','scissors'],
+    "lizard" : ['spock','paper'],
+    "spock" : ['paper','lizard'],
+}
+        self.current_gesture = ''
         self.wins = 0
 
-
+#changed the generate gesture to return a random key for the dicitonary in Player.
     def generate_gesture(self):
-        pass
+        return random.choice(list(self.gestures_win_lose_dict.keys()))
