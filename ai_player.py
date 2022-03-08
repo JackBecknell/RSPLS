3fcf916da5e245a4cfb3f5a0693912fb97c2f599
JackBecknell <52moosefit@gmail.com>
2022-03-08 11:00:26 -0500

    Removed import random as that is now in the parent file. Added a name to AiPlayer 'Bot', and added a status to AiPLayer 'Machine'. Also removed the function for generate gesture as that is in the parent file.

--- ai_player.py
+++ ai_player.py
@@ -1,16 +1,10 @@
 from player import Player
-import random
-
-
 
 class AiPlayer(Player):
     def __init__(self):
-        
+        self.name = 'Bot'
+        self.status = 'Machine'
         super().__init__()
-
-    def generate_gesture(self):
-        for gesture in range(len(self.gesture)):
-            return random.choice(self.gesture)
             
             
 
