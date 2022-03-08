22921c6148b999a467280d92fb0829745537bf67
JackBecknell <52moosefit@gmail.com>
2022-03-08 11:05:09 -0500

    I added a name and a status as instance variables to HumanPlayer. I set the user picks gesture to iterate through the dict stored in the parent function.

--- human_player.py
+++ human_player.py
@@ -1,13 +1,17 @@
+
 from player import Player
 
 class HumanPlayer(Player):
     def __init__(self):
+        self.name = 'Player'
+        self.status = 'Human'
         super().__init__()
-        self.wins = 0
-       
-    def user_picks_gesture(self):
-        user_choice = input(f"Hello User.\nFor {self.gesture[0]} type 0\nFor {self.gesture[1]} type 1\nFor {self.gesture[2]} type 2\nFor {self.gesture[3]} type 3\nFor {self.gesture[4]} type 4\n : ")
-        while user_choice not in ['0','1','2','3','4']:
-            user_choice = input(f"Invlaid Input.\nFor {self.gesture[0]} 0\nFor {self.gesture[1]} type 1\nFor {self.gesture[2]} type 2\nFor {self.gesture[3]} type 3\nFor {self.gesture[4]} type 4\n : ")
-        return self.gesture[int(user_choice)]
+        ##deleted the redundant self.wins = 0 instance variable that was here
 
+#replaced the all the old instance vaiable calls to the new dicitonary calls.
+#note to self. If we wanted to show off that we know how to overwrite instance methods we could name the method below 'generate_gesture' changing it from the parent.
+    def user_picks_gesture(self, pass_name):
+        user_choice = input(f"Hello {pass_name}.\nFor {list(self.gestures_win_lose_dict.keys())[0]} type 0\nFor {list(self.gestures_win_lose_dict.keys())[1]} type 1\nFor {list(self.gestures_win_lose_dict.keys())[2]} type 2\nFor {list(self.gestures_win_lose_dict.keys())[3]} type 3\nFor {list(self.gestures_win_lose_dict.keys())[4]} type 4\n : ")
+        while user_choice not in ['0','1','2','3','4']:
+            user_choice = input(f"Invlaid Input {pass_name}.\nFor {list(self.gestures_win_lose_dict.keys())[0]} 0\nFor {list(self.gestures_win_lose_dict.keys())[1]} type 1\nFor {list(self.gestures_win_lose_dict.keys())[2]} type 2\nFor {list(self.gestures_win_lose_dict.keys())[3]} type 3\nFor {list(self.gestures_win_lose_dict.keys())[4]} type 4\n : ")
+        return (list(self.gestures_win_lose_dict.keys()))[int(user_choice)]
